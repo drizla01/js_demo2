3969675064b9c75335d712619dbf09a21e8e0071
drizla01 <awal4mail@yahoo.com>
2022-06-22 02:58:00 +0100

    javascript introduction

---
+++ file.js
@@ -0,0 +1,8 @@
+const myName = "Awal Umar"
+const height = "170cm"
+const country = "Nigeria"
+
+console.log(myName)
+console.log(height)
+console.log(country)
+//console.log("My name is "+ myName +" I am "+ height +" tall and I am from  "+ country +" ")
